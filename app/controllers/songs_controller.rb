b50dc6f049825ea2c44c24a59ae3db16535c991e
Julianne Kim <juliannne@gmail.com>
2017-11-21 05:52:48 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -21,7 +21,7 @@ class SongsController < ApplicationController
     @song = Song.find_or_create_by(:name => params["Name"])
     @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
     # params["genres"].each { |g| @song.genres << Genre.find_or_create_by(:name => g)}
-    @song.genre_ids = params[:genre]
+    @song.genre_ids = params[:genres]
     @song.save
 
     flash[:message] = "Successfully created song."
